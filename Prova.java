475c9a83dd16cc0765509aa03f48f116b56cc73b
admin <admin@admin.it>
2017-04-19 15:15:47 +0200

    PrimoCommit

---
+++ Prova.java
@@ -0,0 +1,8 @@
+ class Prova { 
+            public static void main(String[] args){ 
+             
+                    System.out.println("Hello World!"); 
+             
+        } 
+     
+    }     
